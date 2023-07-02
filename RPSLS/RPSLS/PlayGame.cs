41e25b5b6783bcf40bd54f6bcd3be56eafa897f1
Liene <Liene@LAPTOP-C1G7S8GH>
2023-07-02 20:24:07 +0300

    Level 3 gameplay split around classes

---
+++ RPSLS/RPSLS/PlayGame.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    public class PlayGame
+    {
+        private GameSettings _settings;
+        private Random _random = new Random();
+        private List<string> _computerNames;
+        private Player _player;
+        private List<string> _winnerList;
+        private int _computerCount;
+
+        public PlayGame()
+        {
+            _settings = new GameSettings(computerCount: 0, roundCount: 0);
+            _computerNames = new List<string>()
+            {
+                "SoftKittyWarmKitty",
+                "RajTheSilent",
+                "Shelbot9000",
+                "StuartTheComicBookGuy",
+                "MoonPie",
+                "SheldorTheConqueror",
+                "ThatsMySpot",
+                "EinsteinVonBrainstorm",
+                "IWentToSpaceOnce"
+            };
+            _player = new Player();
+            _winnerList = new List<string>();
+        }
+
+        public void SetGameSettings(GameSettings settings)
+        {
+            bool isSettingsValid = (settings.ComputerCount > 0 || settings.ComputerCount < 10) &&
+                                   (settings.RoundCount > 0 || settings.RoundCount < 6);
+
+            if (isSettingsValid)
+            {
+                _settings.ComputerCount = settings.ComputerCount;
+                _settings.RoundCount = settings.RoundCount;
+                _computerCount = settings.ComputerCount;
+            }
+        }
+
+        public void RunGame()
+        {
+            for (int c = 0; c < _settings.ComputerCount; c++)
+            {
+                for (int i = 0; i < _settings.RoundCount; i++)
+                {
+                    int playerInput = _player.TakePlayerInput();
+
+                    if (playerInput < 1 || playerInput > 5)
+                    {
+                        Console.WriteLine("Whats the name of your community college?");
+                        Console.WriteLine("GAME OVER");
+                        break;
+                    }
+
+                    Console.WriteLine($"You go with {Turn(playerInput)}");
+
+                    int computerInput = _random.Next(1, 6);
+
+                    Console.WriteLine($"{_computerNames[c]}  goes with {Turn(computerInput)} \n");
+
+                    Console.WriteLine(GetRoundWinner(computerInput, playerInput, _computerNames[c], "player"));
+
+                }
+            }
+            
+            ComputerVsComputer();
+
+            GetGameResults();
+
+            Console.ReadLine();
+        }
+
+        public void ComputerVsComputer()
+        {
+            while (_settings.ComputerCount > 1)
+            {
+                for (int i = 0; i < _settings.ComputerCount - 1; i++)
+                {
+                    for (int j = 0; j < _settings.RoundCount; j++)
+                    {
+                        var computerMove = _random.Next(1, 6);
+
+                        //Console.WriteLine($"{_computerNames[_settings.ComputerCount - 1]} goes with {Turn(computerMove)}");
+
+                        var otherComputerMove = _random.Next(1, 6);
+
+                        //Console.WriteLine($"{_computerNames[i]} goes with {Turn(otherComputerMove)}");
+
+                        var roundWinner = GetRoundWinner(computerMove, otherComputerMove, _computerNames[_settings.ComputerCount - 1],
+                            _computerNames[i]);
+                    }
+                }
+                _settings.ComputerCount--;
+            }
+        }
+
+        private string Turn(int num)
+        {
+            var turn = "";
+
+            switch (num)
+            {
+                case 1:
+                    turn = "Rock";
+                    break;
+                case 2:
+                    turn = "Paper";
+                    break;
+                case 3:
+                    turn = "Scissors";
+                    break;
+                case 4:
+                    turn = "Lizard";
+                    break;
+                case 5:
+                    turn = "Spock";
+                    break;
+            }
+
+            return turn;
+        }
+
+        private string GetRoundWinner(int player1, int player2, string player1Name, string player2Name)
+        {
+            var gameRules = new Dictionary<string, List<string>>
+            {
+                { "Rock", new List<string> { "Scissors", "Lizard" } },
+                { "Paper", new List<string> { "Rock", "Spock" } },
+                { "Scissors", new List<string> { "Paper", "Lizard" } },
+                { "Lizard", new List<string> { "Paper", "Spock" } },
+                { "Spock", new List<string> { "Rock", "Scissors" } }
+            };
+
+            if (player1 == player2)
+            {
+                _winnerList.Add(player1Name);
+                _winnerList.Add(player2Name);
+
+                return "It's a tie! \n";
+            }
+            else if (gameRules[Turn(player2)].Contains((Turn(player1))))
+            {
+                _winnerList.Add(player2Name);
+
+                return $"{player2Name} wins this round! \n";
+            }
+            else
+            {
+                _winnerList.Add(player1Name);
+
+                return $"{player1Name} wins this round! \n";
+            }
+        }
+
+        private void GetGameResults()
+        {
+            Console.WriteLine("The results are in!\n");
+
+            int playerPoints = _winnerList.FindAll(k => k == "player").Count;
+            Console.WriteLine($"Player points: {playerPoints}");
+
+            for (int i = 0; i < _computerCount; i++)
+            {
+                var name = _computerNames[i];
+                int computerPoints = _winnerList.FindAll(k => k == name).Count;
+                Console.WriteLine($"{name} points: {computerPoints}");
+            }
+        }
+    }
+}
--- RPSLS/RPSLS/Program.cs
+++ RPSLS/RPSLS/Program.cs
@@ -4,23 +4,6 @@
     {
         static void Main(string[] args)
         {
-            var random = new Random();
-
-            var computerNames = new List<string>()
-            {
-                "SoftKittyWarmKitty",
-                "RajTheSilent",
-                "Shelbot9000",
-                "StuartTheComicBookGuy",
-                "MoonPie",
-                "SheldorTheConqueror",
-                "ThatsMySpot",
-                "EinsteinVonBrainstorm",
-                "IWentToSpaceOnce"
-            };
-
-            var winnerList = new List<string>();
-
             Console.WriteLine("Welcome to Rock, Paper, Scissors, Lizard, Spock Level 2! \n");
 
             Console.WriteLine("The rules ar obvious: \n" +
@@ -34,152 +17,27 @@
                               "\t Paper disproves Spock,\n" +
                               "\t Spock vaporizes Rock,\n" +
                               "\t and as it always has, Rock crushes Scissors. \n");
-           
+
             Console.WriteLine("Input any number between 1 and 5 \n" +
                               "1: Rock \n" +
                               "2: Paper\n" +
                               "3: Scissors\n" +
                               "4: Lizard\n" +
                               "5: Spock\n");
+            
+            Console.WriteLine("How many computer players (from 1 to 9)?");
+            var computerCount = int.Parse(Console.ReadLine());
 
             Console.WriteLine("How many rounds (from 1 to 5) should players do with each other?");
             var roundCount = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("How many computer players (from 1 to 9)?");
-            var playerCount = int.Parse(Console.ReadLine());
-            var count = playerCount;
-            
-            for (int i = 0; i < playerCount; i++)
-            {
-                for (int j = 0; j < roundCount; j++) 
-                {
-                    Console.WriteLine("Your move!");
-                    int playerInput = int.Parse(Console.ReadLine());
+            var game = new PlayGame();
 
-                    if (playerInput < 1 || playerInput > 5)
-                    {
-                        Console.WriteLine("Whats the name of your community college?");
-                        Console.WriteLine("GAME OVER");
-                        return;
-                    }
-                    
-                    Console.WriteLine($"You go with {Turn(playerInput)}");
+            var settings = new GameSettings(computerCount, roundCount);
 
-                    int computerInput = random.Next(1, 6);
-                    
-                    Console.WriteLine($"{computerNames[i]} goes with {Turn(computerInput)} \n");
+            game.SetGameSettings(settings);
 
-                    var roundWinner = GetRoundWinner(computerInput, playerInput, computerNames[i], "player");
-
-                    if (roundWinner == "tie")
-                    {
-                        winnerList.Add("player");
-                        winnerList.Add($"{computerNames[i]}");
-                    }
-                    else
-                    {
-                        winnerList.Add(roundWinner);
-                    }
-                }
-            }
-            
-            while (playerCount > 1)
-            {
-                for (int i = 0; i < playerCount-1; i++)
-                {
-                    for (int j = 0; j < roundCount; j++)
-                    {
-                        var computerMove = random.Next(1, 6);
-
-                        //Console.WriteLine($"{computerNames[playerCount-1]} goes with {Turn(computerMove)}");
-
-                        var otherComputerMove = random.Next(1, 6);
-
-                        //Console.WriteLine($"{computerNames[i]} goes with {Turn(otherComputerMove)} \n");
-
-                        var roundWinner = GetRoundWinner(computerMove, otherComputerMove, computerNames[playerCount-1],
-                            computerNames[i]);
-
-                        if (roundWinner == "tie")
-                        {
-                            winnerList.Add($"{computerNames[playerCount - 1]}");
-                            winnerList.Add($"{computerNames[i]}");
-                        }
-                        else
-                        {
-                            winnerList.Add(roundWinner);
-                        }
-
-                        //Console.ReadLine();
-                    }
-                }
-                playerCount--;
-            }
-
-            Console.WriteLine("The results are in!\n");
-
-            int playerPoints = winnerList.FindAll(k => k == "player").Count;
-            Console.WriteLine($"Player points: {playerPoints}");
-
-            for (int i = 0; i < count; i++)
-            {
-                var name = computerNames[i];
-                int computerPoints = winnerList.FindAll(k => k == name).Count;
-                Console.WriteLine($"{name} points: {computerPoints}");
-            }
-
-            Console.ReadLine();
-        }
-
-        static string Turn(int num)
-        {
-            var turn = "";
-
-            switch (num)
-            {
-                case 1:
-                    turn = "Rock";
-                    break;
-                case 2:
-                    turn = "Paper";
-                    break;
-                case 3:
-                    turn = "Scissors";
-                    break;
-                case 4:
-                    turn = "Lizard";
-                    break;
-                case 5:
-                    turn = "Spock";
-                    break;
-            }
-
-            return turn;
-        }
-
-        static string GetRoundWinner(int player1, int player2, string player1Name, string player2Name)
-        {
-            var gameRules = new Dictionary<string, List<string>>
-            {
-                { "Rock", new List<string> { "Scissors", "Lizard" } },
-                { "Paper", new List<string> { "Rock", "Spock" } },
-                { "Scissors", new List<string> { "Paper", "Lizard" } },
-                { "Lizard", new List<string> { "Paper", "Spock" } },
-                { "Spock", new List<string> { "Rock", "Scissors" } }
-            };
-
-            if (player1 == player2)
-            {
-                return "tie";
-            }
-            else if (gameRules[Turn(player2)].Contains((Turn(player1))))
-            {
-                return $"{player2Name}";
-            }
-            else
-            {
-                return $"{player1Name}";
-            }
+            game.RunGame();
         }
     }
 }
